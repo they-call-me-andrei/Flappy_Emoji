76cc8bf58cbd3a5f0d3be546f9b7ad27806205b9
Andrei <oranceanuandrei@gmail.com>
2024-03-11 18:26:39 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace game2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(696, 363);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 68);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.ForeColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(-21, -53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(861, 527);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("SimSun", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Gold;
+            this.button2.Location = new System.Drawing.Point(343, 459);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox7.BackgroundImage")));
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(197, 363);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(53, 55);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(69, 350);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(53, 55);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(91, 228);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(53, 55);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(347, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 43);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SCORE : 0";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(47, 74);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(167, 56);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(351, 136);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(167, 56);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(622, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(167, 56);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(318, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Instructions :";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(251, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poor Richard", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(306, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "1.   Use the uparrow to jump.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(306, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "2.   Collect as may points as possible !";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poor Richard", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(306, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "3.  Have fun !";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(564, 413);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 28);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Andrei\'s Development";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(1000, 300);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace game2
+{
+    public partial class Form1 : Form
+    {
+        private static int x = 500, y = 360,x1 = -10,y1 = 300, count = 0;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Top = y;
+            pictureBox1.Left = x;
+            pictureBox4.BringToFront();
+            pictureBox3.BringToFront();
+            pictureBox2.BringToFront();
+            pictureBox1.BringToFront();
+            button2.BringToFront();
+
+            //pictureBox8.BringToFront();    player2
+
+            pictureBox5.BringToFront();
+            pictureBox5.Left = -10;
+            pictureBox5.Top = r.Next(170, 300);
+            pictureBox6.BringToFront();
+            pictureBox6.Left = -150;
+            pictureBox6.Top = r.Next(170, 300);
+            pictureBox7.BringToFront();
+            pictureBox7.Left = -300;
+            pictureBox7.Top = r.Next(170, 300);
+        }
+
+        private static Boolean startgame = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(startgame == true)
+            {
+                if (jump == true)
+                {
+                    pictureBox1.Left -= 2;
+                    pictureBox1.Top = (pictureBox1.Left - duplicate + 5) * (pictureBox1.Left - duplicate + 5) + 5 * (pictureBox1.Left - duplicate + 5) + 80;
+                }
+                if (pictureBox1.Top > 310)
+                {
+                    jump = false;
+                }
+                if (pictureBox1.Left < -20)
+                {
+                    pictureBox1.Left = x + 70;
+                }
+
+                //player2
+
+                /*
+                if (jump1 == true)
+                {
+                    pictureBox8.Left -= 2;
+                    pictureBox8.Top = (pictureBox8.Left - duplicate1 + 5) * (pictureBox8.Left - duplicate1 + 5) + 5 * (pictureBox8.Left - duplicate1 + 5) + 80;
+                }
+                if (pictureBox8.Top > 310)
+                {
+                    jump1 = false;
+                }
+                if (pictureBox8.Left < -70)
+                {
+                    pictureBox8.Left = x + 200;
+                }
+                */
+            }
+           
+            
+        }
+
+        private static int duplicate,duplicate1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            button1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            startgame = true;
+        }
+
+        private static int pause = 0;
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if(pause %2 == 0)
+            {
+                startgame = false;
+                button2.Text = "Unpause";
+            }
+            else
+            {
+                startgame = true;
+                button2.Text = "Pause";
+            }
+            pause++;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {/*
+            button2.Width += 10;
+            button2.Height += 10;
+            button2.Location = new Point(button2.Left - 10, button2.Top - 10);*/
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {/*
+            button2.Width -= 10;
+            button2.Height -= 10;
+            button2.Location = new Point(button2.Left + 10, button2.Top + 10);
+            */
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (x % 2 == 0)
+            {
+                startgame = false;
+                button2.Text = "Unpause";
+            }
+            else
+            {
+                startgame = true;
+                button2.Text = "Pause";
+            }
+            x++;
+        }
+
+        private static Boolean hit = false;
+
+
+        private static Boolean jump = false, jump1 = false;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(startgame == true)
+            {
+                pictureBox5.Left += 2;
+                pictureBox6.Left += 2;
+                pictureBox7.Left += 2;
+                if (pictureBox5.Left > 610)
+                {
+                    pictureBox5.Left = -150;
+                    pictureBox5.Top = r.Next(170, 300);
+                }
+                if (pictureBox6.Left > 610)
+                {
+                    pictureBox6.Left = -250;
+                    pictureBox6.Top = r.Next(170, 300);
+                }
+                if (pictureBox7.Left > 610)
+                {
+                    pictureBox7.Left = -400;
+                    pictureBox7.Top = r.Next(170, 300);
+                }
+                if ((pictureBox1.Left + 72>pictureBox5.Left)&&(pictureBox1.Left<pictureBox5.Left + 42))
+                {
+                    if((pictureBox1.Top + 68>pictureBox5.Top)&&(pictureBox1.Top<pictureBox5.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox5.Left = -150;
+                        pictureBox5.Top = r.Next(170, 300);
+                    }
+                }
+                if ((pictureBox1.Left + 72 > pictureBox6.Left) && (pictureBox1.Left < pictureBox6.Left + 42))
+                {
+                    if ((pictureBox1.Top + 68 > pictureBox6.Top) && (pictureBox1.Top < pictureBox6.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox6.Left = -250;
+                        pictureBox6.Top = r.Next(170, 300);
+                    }
+                }
+                if ((pictureBox1.Left + 72 > pictureBox7.Left) && (pictureBox1.Left < pictureBox7.Left + 42))
+                {
+                    if ((pictureBox1.Top + 68 > pictureBox7.Top) && (pictureBox1.Top < pictureBox7.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox7.Left = -400;
+                        pictureBox7.Top = r.Next(170, 300);
+                    }
+                }
+
+                //player2
+                /*
+                if ((pictureBox8.Left + 72 > pictureBox5.Left) && (pictureBox8.Left < pictureBox5.Left + 42))
+                {
+                    if ((pictureBox8.Top + 68 > pictureBox5.Top) && (pictureBox8.Top < pictureBox5.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox5.Left = -150;
+                        pictureBox5.Top = r.Next(170, 300);
+                    }
+                }
+                if ((pictureBox8.Left + 72 > pictureBox6.Left) && (pictureBox8.Left < pictureBox6.Left + 42))
+                {
+                    if ((pictureBox8.Top + 68 > pictureBox6.Top) && (pictureBox8.Top < pictureBox6.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox6.Left = -250;
+                        pictureBox6.Top = r.Next(170, 300);
+                    }
+                }
+                if ((pictureBox8.Left + 72 > pictureBox7.Left) && (pictureBox8.Left < pictureBox7.Left + 42))
+                {
+                    if ((pictureBox8.Top + 68 > pictureBox7.Top) && (pictureBox8.Top < pictureBox7.Top + 44))
+                    {
+                        hit = true;
+                        pictureBox7.Left = -400;
+                        pictureBox7.Top = r.Next(170, 300);
+                    }
+                }*/
+                if (hit == true)
+                {
+                    count++;
+                    label6.Text = "Score : " + count;
+                    hit = false;
+
+                }
+            }
+            
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if((keyData == Keys.Up)&&(startgame == true))
+            {
+                duplicate = pictureBox1.Left;
+                jump = true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData) ;
+        }
+        
+     }
+}
+
